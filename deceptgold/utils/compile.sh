94d45c6e7235b9ab06db7bc5c82238e6f98ed166
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-04-07 22:56:11 -0300

    doc: refact the description and more.

--- deceptgold/utils/compile.sh
+++ deceptgold/utils/compile.sh
@@ -5,6 +5,7 @@ poetry run pyarmor gen -O src_obf -r -i src/deceptgold
 poetry run briefcase build -u
 poetry run briefcase package
 rm -rf src_obf
+rm -rf src/deceptgold.dist-info
 echo "================================================================================="
 echo "Use the files in the dist directory as a packaged system, ready for distribution."
 echo "================================================================================="
