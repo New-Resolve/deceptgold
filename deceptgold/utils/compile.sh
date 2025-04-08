eb18cc0b6a34edfb3f12ce01f72d20a1978c12a3
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-04-08 00:32:02 -0300

    fix: filename .deb distribuition

--- deceptgold/utils/compile.sh
+++ deceptgold/utils/compile.sh
@@ -3,7 +3,7 @@
 . $(poetry env info --path)/bin/activate
 poetry run pyarmor gen -O src_obf -r -i src/deceptgold
 poetry run briefcase build -u
-poetry run briefcase package
+poetry run briefcase package -v
 rm -rf src_obf
 rm -rf src/deceptgold.dist-info
 echo "================================================================================="
