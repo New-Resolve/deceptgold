956c02394418c142bd096635c31d13482c1d3617
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-04-17 15:28:14 -0300

    fix: chanched cripto signature to ERC20

--- deceptgold/src/deceptgold/help/signature.py
+++ deceptgold/src/deceptgold/help/signature.py
@@ -1,36 +1,27 @@
 import json
-import hashlib
-from ecdsa import SigningKey, VerifyingKey
+from eth_utils import keccak
+from eth_account.messages import encode_defunct
+from eth_account import Account
 
-# nose: B105 - Intentional use of hardcoded key for cryptographic signature
-def generate_signature_and_hash(json_honeypot):
-    """
-    WARNING: INTENTIONAL USE OF HARDCODED PRIVATE KEY
-    This private key is purposefully embedded to ensure that
-    the honeypot generates consistent and verifiable signatures by a smart contract.
-    The security of the key is guaranteed by other means such as via pyarmor
+def generate_new_keys():
+    account = Account.create()
+    private_key = account.key.hex()
+    public_address = account.address
+    print("New Private Key:", private_key)
+    print("New Public Key:", public_address)
 
-    ATTENTION: Change private key when repository goes to new private version control.
-    """
-    PRIVATE_KEY_PEM = """-----BEGIN EC PRIVATE KEY-----
-    MHQCAQEEILMae4Sd8YfFS1lVru4tE5NK/6z6fEGsRuM0j4BxQ+RHoAcGBSuBBAAKoUQDQgAENEN0
-    GIdlOh4ufpvZz2m7t7xBPgP7nz8v2cB3iaT91HBeamJTIbajCXQY8/g4FBNXV0wiGbb2Mn8Km4se
-    6vftbw==
-    -----END EC PRIVATE KEY-----"""
-    private_key = SigningKey.from_pem(PRIVATE_KEY_PEM)
-    json_string = json.dumps(json_honeypot, separators=(',', ':'), sort_keys=True)
-    json_hash = hashlib.sha256(json_string.encode()).digest()
-    signature = private_key.sign(json_hash)
-    return signature, json_hash
 
-def validate_signature_and_hash(signature_and_hash: tuple[bytes, bytes]):
-    PUBLIC_KEY_PEM = """-----BEGIN PUBLIC KEY-----
-    MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAENEN0GIdlOh4ufpvZz2m7t7xBPgP7nz8v2cB3iaT91HBe
-    amJTIbajCXQY8/g4FBNXV0wiGbb2Mn8Km4se6vftbw==
-    -----END PUBLIC KEY-----"""
-    public_key = VerifyingKey.from_pem(PUBLIC_KEY_PEM)
-    try:
-        public_key.verify(signature_and_hash[0], signature_and_hash[1])
-        return True
-    except Exception:
-        return False
+PRIVATE_KEY = "519216cdca0785ea8f01906bb177a987ad8b0181c7dec347f5c0663e8170fb43"
+EXPECTED_ADDRESS = "0xfA6a145a7e1eF7367888A39CBf68269625C489D2"
+
+def generate_signature_and_hash(json_data):
+    json_string = json.dumps(json_data, separators=(',', ':'), sort_keys=True)
+    message_hash = keccak(text=json_string)
+    message = encode_defunct(message_hash)
+    signed_message = Account.sign_message(message, private_key=PRIVATE_KEY)
+    return signed_message.signature, message_hash, json_string
+
+def verify_signature(signature, message_hash, expected_address=EXPECTED_ADDRESS):
+    message = encode_defunct(message_hash)
+    recovered_address = Account.recover_message(message, signature=signature)
+    return recovered_address == expected_address
--- deceptgold/tests/test_signature.py
+++ deceptgold/tests/test_signature.py
@@ -1,7 +1,34 @@
-from deceptgold.help.signature import generate_signature_and_hash, validate_signature_and_hash
+from deceptgold.help.signature import generate_signature_and_hash, verify_signature
+
+def get_list_hash_honeypot():
+    return [
+        {"dst_host": "127.0.0.1", "dst_port": 2223, "local_time": "2025-04-17 16:56:29.835081",
+         "local_time_adjusted": "2025-04-17 13:56:29.835109",
+         "logdata": {"LOCALVERSION": "SSH-2.0-OpenSSH_5.1p1 Debian-4", "PASSWORD": "admin",
+                     "REMOTEVERSION": "SSH-2.0-OpenSSH_9.2p1 Debian-2+deb12u5", "USERNAME": "root"},
+         "logtype": 4002, "node_id": "opencanary-1", "src_host": "127.0.0.1", "src_port": 33374,
+         "utc_time": "2025-04-17 16:56:29.835101"},
+        {"dst_host": "127.0.0.1", "dst_port": 22, "local_time": "2025-04-17 16:56:29.824626",
+         "local_time_adjusted": "2025-04-17 13:56:29.824645", "logdata": {"SESSION": "158"}, "logtype": 4000,
+         "node_id": "opencanary-1", "src_host": "127.0.0.1", "src_port": 33482,
+         "utc_time": "2025-04-17 16:56:29.824641"}
+
+    ]
+def test_signature_and_hash_simple():
+    request_honeypot = {"dst_host": "127.0.0.1", "dst_port": 21, "local_time": "2025-04-17 16:56:29.789537", "local_time_adjusted": "2025-04-17 13:56:29.789555", "logdata": {"SESSION": "154"}, "logtype": 4000, "node_id": "opencanary-1", "src_host": "127.0.0.1", "src_port": 33382, "utc_time": "2025-04-17 16:56:29.789551"}
+    signature, json_hash, json_string = generate_signature_and_hash(request_honeypot)
+    assert verify_signature(signature, json_hash)
 
 
-def test_signature_and_hash():
-    json_honeypot = {"a": 1}
-    sig_hash = generate_signature_and_hash(json_honeypot)
-    assert validate_signature_and_hash(sig_hash)
+def test_signature_and_hash_list():
+    list_honeypot = get_list_hash_honeypot()
+    for i, request_honeypot in enumerate(list_honeypot, start=1):
+        signature, json_hash, json_string = generate_signature_and_hash(request_honeypot)
+        is_valid = verify_signature(signature, json_hash)
+        if not is_valid:
+            print(f"\n❌ Assinatura inválida no item {i}!")
+            print("JSON:", json_string)
+            print("Assinatura:", signature.hex())
+            print("Hash:", json_hash.hex())
+            assert False, f"Assinatura inválida no item {i} - abortando o teste!"
+    assert True
