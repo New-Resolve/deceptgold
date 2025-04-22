589789366f73a145b825f5ca66abf102a4cbe54a
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-04-21 22:28:14 -0300

    ci: changed default title changelog.

--- deceptgold/utils/create_changelog.py
+++ deceptgold/utils/create_changelog.py
@@ -1,6 +1,11 @@
 import subprocess
-import sys
+
 
 def main():
-    command = "git-cliff > src/deceptgold/CHANGELOG"
-    subprocess.run(command, shell=True, check=True)
+    result = subprocess.run("git-cliff", shell=True, check=True, capture_output=True, text=True)
+    output = result.stdout.replace("unreleased", "Last Version")
+    with open("src/deceptgold/CHANGELOG", "w") as f:
+        f.write(output)
+
+if __name__ == "__main__":
+    main()
