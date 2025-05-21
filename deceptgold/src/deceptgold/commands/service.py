a38792bb88dfd8f8795e5572df38a9f430b59f6e
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-05-21 16:03:01 -0300

    react: correct import of internal module due to creation of module for function reuse; fix: fix in the start command call to accept arguments from the daemon and force-no-wallet parameters; refact: Generic execution creation.

--- deceptgold/src/deceptgold/commands/service.py
+++ deceptgold/src/deceptgold/commands/service.py
@@ -6,7 +6,10 @@ import sys
 
 from cyclopts import App
 from deceptgold.configuration.opecanary import generate_config
-from deceptgold.help.opencanary.help_opencanary import start_opencanary_internal
+from deceptgold.configuration.config_manager import get_config
+from deceptgold.helper.opencanary.help_opencanary import start_opencanary_internal
+from deceptgold.helper.helper import parse_args
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,32 +26,56 @@ def pre_execution():
 def pre_execution_decorator(func):
     def wrapper(*args, **kwargs):
         pre_execution()
-        return func(*args, **kwargs)
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            print("Error making encapsulated call. Check the documentation as the command is apparently being sent incorrectly.")
     return wrapper
 
-
-@services_app.command(name="start", help="Start service(s) in operational system")
+@services_app.command(name="start", help=(
+        "Start service(s) in operational system\n\n"
+        "Warning:\n"
+        "  To execute this command you must inform your public wallet address to receive your rewards.\n\n"
+        "Verbosity:\n"
+        "  daemon=true|false              Run in background (default: true)\n\n"
+        "Forced arguments:\n"
+        "  force-no-wallet=true|false     Skip wallet verification (default: false)\n\n"
+        "Usage example:\n"
+        "  deceptgold service start daemon=false force-no-wallet=true"
+    ))
 @pre_execution_decorator
-def start(
-        daemon: bool = False, force_no_wallet: bool = False,
-):
+def start(*args):
+    parsed_args = parse_args(args)
+
+    daemon = parsed_args.get('daemon', True)
+    force_no_wallet = parsed_args.get('force_no_wallet', False)
+
+    p_force_no_wallet = ''
+    if force_no_wallet:
+        p_force_no_wallet = 'force_no_wallet=True'
+
+    if get_config("user", "address", None) is None:
+        if daemon and not force_no_wallet:
+            print(f"The current user has not configured their public address to receive their rewards. The system no will continue. It is recommended to configure it before starting the fake services. Use the force-no-wallet=true parameter to continue without system interruption. But be careful, you will not be able to redeem your rewards now and retroactively.")
+            sys.exit(1)
 
     msg_already_run = "The service is already running. Consider using the 'stop' command to stop it from running if necessary."
     if not daemon:
         if os.path.exists(PID_FILE):
             logger.warning(msg_already_run)
             print(msg_already_run)
-            return
-        start_opencanary_internal(force_no_wallet)
+            return None
+        start_opencanary_internal(p_force_no_wallet)
     else:
         if os.path.exists(PID_FILE):
             logger.warning(msg_already_run)
             print(msg_already_run)
-            return
+            return None
+
         python_exec = sys.executable
         with open(LOG_FILE, "w") as out:
             process = subprocess.Popen(
-                [python_exec, "-m", "deceptgold.entrypoints.opencanary_runner"],
+                [python_exec, "-m", "deceptgold.entrypoints.opencanary_runner", p_force_no_wallet],
                 stdout=out,
                 stderr=out,
                 start_new_session=True,
@@ -70,5 +97,7 @@ def stop():
         logger.info(f"Process {pid} finished executing.")
     except ProcessLookupError:
         logger.warning(f"Process {pid} not found.")
+    except Exception as e:
+        logger.warning("Error in stop deceptgold: {e}")
     finally:
         os.remove(PID_FILE)
