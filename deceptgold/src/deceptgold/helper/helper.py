dc935c25a90f6b00b11e558a825263c41bbae9b3
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-05-22 00:53:35 -0300

    fix: add check if variable exists.

--- deceptgold/src/deceptgold/helper/helper.py
+++ deceptgold/src/deceptgold/helper/helper.py
@@ -1,21 +1,21 @@
 def parse_args(args):
     parsed = {}
+    if args:
+        if not args[0] is None:
+            for arg in args:
+                if '=' not in arg:
+                    continue
 
-    if not args[0] is None:
-        for arg in args:
-            if '=' not in arg:
-                continue
+                key, value = arg[:].split('=', 1)
 
-            key, value = arg[:].split('=', 1)
+                key = key.replace('-', '_').strip().lower()
+                value = value.strip().lower()
 
-            key = key.replace('-', '_').strip().lower()
-            value = value.strip().lower()
-
-            if value in ('true', '1', 'yes', 'on'):
-                parsed[key] = True
-            elif value in ('false', '0', 'no', 'off'):
-                parsed[key] = False
-            else:
-                parsed[key] = value
+                if value in ('true', '1', 'yes', 'on'):
+                    parsed[key] = True
+                elif value in ('false', '0', 'no', 'off'):
+                    parsed[key] = False
+                else:
+                    parsed[key] = value
 
     return parsed
