c55e12cd0d94bfe8dc56dcb1b7d10b201e959598
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-06-01 01:01:38 -0300

    feat: new module notify user.

--- deceptgold/src/deceptgold/__main__.py
+++ deceptgold/src/deceptgold/__main__.py
@@ -18,6 +18,7 @@ from deceptgold.helper.opencanary.proxy_logger import CustomFileHandler
 try:
     from deceptgold.commands.user import users_app
     from deceptgold.commands.service import services_app
+    from deceptgold.commands.notify import notify_app
     from deceptgold.configuration import log
 except ImportError as e:
     pass # print(e)
@@ -25,6 +26,7 @@ except ImportError as e:
 try:
     from deceptgold.src.deceptgold.commands.user import users_app
     from deceptgold.src.deceptgold.commands.service import services_app
+    from deceptgold.src.deceptgold.commands.notify import notify_app
     from deceptgold.src.deceptgold.configuration.log import *
 except ImportError as e:
     pass # print(e)
@@ -35,6 +37,8 @@ logging.FileHandler = CustomFileHandler
 app = App(name="DeceptGold", help=get_description())
 app.command(users_app)
 app.command(services_app)
+app.command(notify_app)
+
 
 multiprocessing.set_start_method("spawn")
 
