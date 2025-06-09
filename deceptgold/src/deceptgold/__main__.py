af5e1c4166483867cd42ef5aae4e5b3a80fcb47f
jonathan.morais <jonathan.morais@cyberlabs.com.br>
2025-06-09 19:00:20 -0300

    refact: modify layout entry point to general messages exceptions.

--- deceptgold/src/deceptgold/__main__.py
+++ deceptgold/src/deceptgold/__main__.py
@@ -3,53 +3,56 @@ import timeit
 import warnings
 import logging
 import multiprocessing
+import traceback
 
 from cyclopts import App
 from cryptography.utils import CryptographyDeprecationWarning
+
 warnings.filterwarnings("ignore", category=ResourceWarning)
 warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning, module="eth_utils.functional")
 warnings.filterwarnings("ignore", category=SyntaxWarning)
 
-from deceptgold.helper.descripton import get_description
-from deceptgold.helper.opencanary.proxy_logger import CustomFileHandler
+def init_app():
+    try:
+        if not hasattr(time, "clock"):
+            time.clock = timeit.default_timer
 
-try:
-    from deceptgold.commands.user import users_app
-    from deceptgold.commands.service import services_app
-    from deceptgold.commands.notify import notify_app
-    from deceptgold.commands.server import server_app
-    from deceptgold.configuration import log
-except ImportError as e:
-    pass # print(e)
+        from deceptgold.helper.descripton import get_description
+        from deceptgold.helper.opencanary.proxy_logger import CustomFileHandler
+        from deceptgold.helper.helper import my_self_developer
+        from deceptgold.commands.user import users_app
+        from deceptgold.commands.service import services_app
+        from deceptgold.commands.notify import notify_app
+        from deceptgold.configuration import log
 
-try:
-    from deceptgold.src.deceptgold.commands.user import users_app
-    from deceptgold.src.deceptgold.commands.service import services_app
-    from deceptgold.src.deceptgold.commands.notify import notify_app
-    from deceptgold.src.deceptgold.commands.server import server_app
-    from deceptgold.src.deceptgold.configuration.log import *
-except ImportError as e:
-    pass # print(e)
+        logger = logging.getLogger(__name__)
+        logging.FileHandler = CustomFileHandler
+        logger.info("Initialization complete the application.")
 
-logger = logging.getLogger(__name__)
-logging.FileHandler = CustomFileHandler
+        app = App(name="DeceptGold", help=get_description())
+        app.command(users_app)
+        app.command(services_app)
+        app.command(notify_app)
 
-app = App(name="DeceptGold", help=get_description())
-app.command(users_app)
-app.command(services_app)
-app.command(notify_app)
-app.command(server_app)
+        app()
 
+        if logger:
+            logger.info("Finally application!")
 
-multiprocessing.set_start_method("spawn")
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        print(f"Critical error in the application. Please contact us to report this situation. We do not collect any "
+              f"information. It would be necessary for you to send us specific information about this specific "
+              f"situation. Help us to constantly improve. contact@decept.gold")
+        if my_self_developer():
+            traceback.print_exc()
+
+    multiprocessing.set_start_method("spawn")
+
 
 
 if __name__ == "__main__":
-    if not hasattr(time, "clock"):
-        time.clock = timeit.default_timer
-
-    logger.info("Initialization complete the application.")
-    app()
-    logger.info("Finally application!")
+    init_app()
